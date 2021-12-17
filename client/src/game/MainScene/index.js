0e13a1822c99b6e6c4d3d13b613a4d599ed51e49
dMosca <devinmosca@yahoo.com>
2021-12-17 14:30:42 -0700

    Reverted back to sprites working, Added test sprites again, created player folder

--- client/src/game/MainScene/index.js
+++ client/src/game/MainScene/index.js
@@ -1,10 +1,14 @@
 import Phaser from "phaser";
-import { Scene } from "phaser";
+// import { Scene } from "phaser";
 
-import spriteAtlas from "../assets/images/test_sprite2_atlas.json";
-import testSprite from "../assets/images/test_sprite2.png";
-// import spriteAtlas from "../assets/images/knight-sprite.json";
-// import testSprite from "../assets/images/knight-sprite.png";
+import spriteAtlas from '../assets/images/test_sprite_atlas.json'
+import testSprite from '../assets/images/test_sprite.png'
+// import spriteAtlas2 from '../assets/images/test_sprite2_atlas.json'
+// import testSprite2 from '../assets/images/test_sprite2.png'
+// import knightAtlas from '../assets/images/knight-sprite.json'
+// import knightSprite from '../assets/images/knight-sprite.png'
+// import sobAtlas from '../assets/images/atlas.json'
+// import sobSprite from '../assets/images/atlas.png'
 
 export default class MainScene extends Phaser.Scene {
   constructor() {
@@ -13,19 +17,22 @@ export default class MainScene extends Phaser.Scene {
 
   preload() {
     console.log("preload");
-    this.load.spritesheet('test_sprite2', testSprite, spriteAtlas)
+    this.load.atlas('test_sprite', testSprite, spriteAtlas)
     console.log(testSprite, spriteAtlas)
+    // this.load.atlas('test_sprite2', testSprite2, spriteAtlas2)
+    // console.log(testSprite2, spriteAtlas2)
+    // this.load.atlas('knight-sprite', knightSprite, knightAtlas)
+    // console.log(knightSprite, knightAtlas)
+    // this.load.atlas('atlas', testSprite, spriteAtlas)
+    // console.log(sobSprite, sobAtlas)
   }
 
   create() {
     console.log("create");
-    this.player = new Phaser.Physics.Matter.Sprite(
-      this.matter.world,
-      400,
-      300,
-      "atlas",
-      "knight_idle_1"
-    );
+    this.player = new Phaser.Physics.Matter.Sprite(this.matter.world,10,50,'test_sprite');
+    // this.player = new Phaser.Physics.Matter.Sprite(this.matter.world,10,50,'test_sprite2');
+    // this.player = new Phaser.Physics.Matter.Sprite(this.matter.world,10,50,'knight-sprite');
+    // this.player = new Phaser.Physics.Matter.Sprite(this.matter.world,10,50,'atlas');
     this.add.existing(this.player);
     this.inputKeys = this.input.keyboard.addKeys({
       up: Phaser.Input.Keyboard.KeyCodes.W,
